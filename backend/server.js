458d2f230f7479748511ceaf9ee47dc82d1ee86b
Andrew <andrew@conducttr.com>
2020-12-29 19:45:27 +0000

    Adding ModelsisDelivered: {

--- backend/server.js
+++ backend/server.js
@@ -1,9 +1,13 @@
-const express = require('express')
-const products = require('./data/products')
-const dotenv = require('dotenv')
+import express  from 'express'
+import products from'./data/products.js'
+import dotenv from 'dotenv'
+import connectDB from './config/db.js'
+
 const app = express()
 
 dotenv.config()
+
+connectDB()
 
 app.get('/' , (req, res) =>{
     res.send('API is up and running')
