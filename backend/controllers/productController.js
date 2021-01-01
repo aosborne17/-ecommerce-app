1f440c355d0671f461826a2fdf3e20c9fbaae3b7
Andrew <andrew@conducttr.com>
2021-01-01 13:43:52 +0000

    Creating controller folder to handle the functionality

--- backend/controllers/productController.js
+++ backend/controllers/productController.js
@@ -1,2 +1,30 @@
-import express from 'express'
 import asyncHandler from 'express-async-handler'
+import Product from '../models/productModel.js'
+
+
+// @description  Fetch all products
+// @route  GET /api/products
+// @access Public
+
+const getProducts = asyncHandler(async (req, res) => {
+    // by passing in an empty project we want to get all the products from the db
+    const products = await Product.find({})
+    res.json(products)
+})
+
+
+const getProductById = asyncHandler(async (req, res) => {
+    const product = await Product.findById(req.params.id)
+    if (product) {
+        res.json(product)
+    } else {
+        res.status(404)
+        // This will be fired when the product ID from the url is in the correct format, however it doesn't exist in our DB
+        throw new Error('Product Not Found')
+    }
+})
+
+export {
+    getProducts,
+    getProductById,
+}
--- backend/routes/productRoutes.js
+++ backend/routes/productRoutes.js
@@ -1,35 +1,14 @@
 import express from 'express'
-import asyncHandler from 'express-async-handler'
 const router = express.Router()
-import Product from '../models/productModel.js'
+import {getProducts, getProductById} from '../controllers/productController.js'
 
 
 
-// @description  Fetch all products
-// @route  GET /api/products
-// @access Public
+// All the functionality has been moved to the controller folders
 
-router.get('/' , asyncHandler(async (req, res) =>{
-    // by passing in an empty project we want to get all the products from the db
-    const products = await Product.find({})
-    res.json(products)
-}))
+// The routes folder simply directs the request to the controller, so it can be handled
+router.get('/', getProducts)
+router.get('/:id', getProductById)
 
-
-// @description  Fetch single product
-// @route  GET /api/products/:id
-// @access Public
-
-router.get('/:id' , asyncHandler(async (req, res) =>{
-    const product = await Product.findById(req.params.id)
-    if (product) {
-        res.json(product)
-    } else {
-        res.status(404)
-        // This will be fired when the product ID from the url is in the correct format, however it doesn't exist in our DB
-        throw new Error('Product Not Found')
-    }
-
-}))
 
 export default router
